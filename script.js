3ee2b8fb1770a3a5d96137c129dbdbcf48f90aff
sonjasvensk <162495225+sonjasvensk@users.noreply.github.com>
2025-04-23 15:12:59 +0300

    taulukot, ukkeli

--- script.js
+++ script.js
@@ -20,98 +20,173 @@
 //      Jos virheiden määrä saavuttaa maksimin (kun ukkeli piirretty):
 //          Näytä "Hävisit! Sana oli: XXXXX" & esim. alerttina pääkallo/hautakivi tms.
 
-
 //  -piirräSeuraava()
 
-let sanat = [relaatiotietokanta, ratkaisuarkkitehti, heuristiikka, palvelumuotoilu, parametri, välimuisti, sovellusvirtualisointi, javascript, projektipäällikkö, analytiikka];
-let kirjaimet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, x, y, z, å, ä, ö];
+let sanat = [
+  "relaatiotietokanta",
+  "ratkaisuarkkitehti",
+  "heuristiikka",
+  "palvelumuotoilu",
+  "parametri",
+  "välimuisti",
+  "sovellusvirtualisointi",
+  "javascript",
+  "projektipäällikkö",
+  "analytiikka",
+];
+let kirjaimet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "x",
+  "y",
+  "z",
+  "å",
+  "ä",
+  "ö",
+];
 let väärinarvatut = "";
 let arvaukset = "";
 let arvattavaSana = "";
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
+function uusiPeli(arvattavaSana) {
+  // Tyhjentää pelikentän, hakee pelattavan sanan + piirtää puun
 
+  //puu
+  ctx.strokeStyle = "black";
+  ctx.beginPath();
+  ctx.moveTo(20, 300);
+  ctx.lineTo(300, 300);
+  ctx.moveTo(200, 5);
+  ctx.lineTo(200, 300);
+  ctx.moveTo(90, 5);
+  ctx.lineTo(200, 5);
+  ctx.stroke();
+  ctx.closePath();
+}
 
-function uusiPeli() { // Tyhjentää pelikentän, hakee pelattavan sanan + piirtää puun
-    
-    //puu
-    ctx.strokeStyle = "black";
-    ctx.beginPath();
-    ctx.moveTo(20, 300)
-    ctx.lineTo(300, 300)
-    ctx.moveTo(200, 5)
-    ctx.lineTo(200, 300)
-    ctx.moveTo(90, 5)
-    ctx.lineTo(200, 5)
-    ctx.stroke();
-    ctx.closePath();
-    }
-
-function arvattavaSana() {
-    
+function valittuSana() {
+  // vaihdoin tämän funktion nimen, koska oli ongelmallista, että myös muuttujan nimi oli sama
+  arvattavaSana = sanat[Math.floor(Math.random() * sanat.length)];
+  uusiPeli(arvattavaSana);
 }
 function arvaaSana() {
-    let arvattuSana = document.getElementById("arvattuSana").value;
-    let result = arvattuSana.toLowerCase().trim();
-    if (result == arvattavaSana) {
-        voitto(arvattavaSana);
-    }
-    else {
-        gameOver(arvattavaSana);
-    }
+  let arvattuSana = document.getElementById("arvattuSana").value;
+  let result = arvattuSana.toLowerCase().trim();
+  if (result == arvattavaSana) {
+    voitto(arvattavaSana);
+  } else {
+    gameOver(arvattavaSana);
+  }
 }
-function arvaaKirjain(arvattavaSana) { // tarkistaa että kirjain löytyykö kirjain sanasta EI ja KYLLÄ
-    let kirjain = document.getElementById("kirjain").value;
-    let syote = kirjain.toLowerCase(); 
-      //  kirjaimenTarkistus();
-    arvaukset.push(syote);
-    let tarkiste=""; 
-    for (let i = 0; i < arvattavaSana.length; i++) {
-        if (syote == arvattavaSana[i]) {
-            document.getElementById("").innerHTML= arvattavaSana[i];
-            tarkiste="OK";
-        }
+function arvaaKirjain(arvattavaSana) {
+  // tarkistaa että kirjain löytyykö kirjain sanasta EI ja KYLLÄ
+  let kirjain = document.getElementById("kirjain").value;
+  let syote = kirjain.toLowerCase();
+  //  kirjaimenTarkistus();
+  arvaukset.push(syote);
+  let tarkiste = "";
+  for (let i = 0; i < arvattavaSana.length; i++) {
+    if (syote == arvattavaSana[i]) {
+      document.getElementById("").innerHTML = arvattavaSana[i];
+      tarkiste = "OK";
     }
-    document.getElementById("").innerHTML = syote;
-    if (tarkiste == "OK") {
-        sananTarkistus(arvattavaSana);
-    } else {
-        piirräUkko();
-    }
+  }
+  document.getElementById("").innerHTML = syote;
+  if (tarkiste == "OK") {
+    sananTarkistus(arvattavaSana);
+  } else {
+    piirräUkko();
+  }
 }
 
-function piirräUkko() { // piirtää palasen  
-
+function piirräUkko() {
+  // piirtää palasen
+  //köysi
+  ctx.beginPath();
+  ctx.moveTo(100, 5);
+  ctx.lineTo(100, 51);
+  ctx.stroke();
+  ctx.closePath();
+  ctx.beginPath();
+  //pää
+  ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.closePath();
+  ctx.beginPath();
+  //kroppa
+  ctx.moveTo(100, 150);
+  ctx.lineTo(100, 220);
+  ctx.stroke();
+  //vasen jalka
+  ctx.moveTo(100, 220);
+  ctx.lineTo(70, 270);
+  ctx.stroke();
+  //oikea jalka
+  ctx.moveTo(100, 220);
+  ctx.lineTo(130, 270);
+  ctx.stroke();
+  //vasen käsi
+  ctx.moveTo(100, 170);
+  ctx.lineTo(50, 150);
+  ctx.stroke();
+  //oikea käsi
+  ctx.moveTo(100, 170);
+  ctx.lineTo(150, 150);
+  ctx.stroke();
 }
 
 // function kirjaimenTarkistus() {
 
 //}
 
-function sananTarkistus(arvattavaSana) { //tarkistaa onko kaikki oikeat kirjaimet löydetty
-    let a = 0;
-    for(let i= 0; i < arvattavaSana.length; i++) { // käy kaikki sanan kirjaimet läpi yksitellen
-        for (let i = 0; i < arvaukset.length; i++) { 
-            if (arvattavasana[i] == arvaukset[i]) { // tarkistaa kuinka moni kirjain löytyy jo arvatut taulukosta
-                a++; // laskuri laskee kaikki kirjaimet jotka on jo arvattu 
-            }
-        }
+function sananTarkistus(arvattavaSana) {
+  //tarkistaa onko kaikki oikeat kirjaimet löydetty
+  let a = 0;
+  for (let i = 0; i < arvattavaSana.length; i++) {
+    // käy kaikki sanan kirjaimet läpi yksitellen
+    for (let i = 0; i < arvaukset.length; i++) {
+      if (arvattavasana[i] == arvaukset[i]) {
+        // tarkistaa kuinka moni kirjain löytyy jo arvatut taulukosta
+        a++; // laskuri laskee kaikki kirjaimet jotka on jo arvattu
+      }
     }
-    if (a == arvattavaSana.length) { //vertaa laskurin arvoa arvattavan sanan pituuteen
-        voitto(arvattavaSana); // Mikäli kaikki kirjaimet oli jo arvattu --> voitto
-    }
-    else {
-        return;
-    }
+  }
+  if (a == arvattavaSana.length) {
+    //vertaa laskurin arvoa arvattavan sanan pituuteen
+    voitto(arvattavaSana); // Mikäli kaikki kirjaimet oli jo arvattu --> voitto
+  } else {
+    return;
+  }
 }
 
-function gameOver(arvattavaSana) { // 1. Jos arvaa sanan väärin 2. Jos arvaa kirjaimet väärin
-alert("Hävisit pelin!");
+function gameOver(arvattavaSana) {
+  // 1. Jos arvaa sanan väärin 2. Jos arvaa kirjaimet väärin
+  alert("Hävisit pelin!");
 }
 
-function voitto(arvattavaSana) { // 1. Jos arvaa sanan oikein 2. Jos arvaa kirjaimet oikein
-alert("Onneksi olkoon! Voitit pelin!");
+function voitto(arvattavaSana) {
+  // 1. Jos arvaa sanan oikein 2. Jos arvaa kirjaimet oikein
+  alert("Onneksi olkoon! Voitit pelin!");
 }
-
-
